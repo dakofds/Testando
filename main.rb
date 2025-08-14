f2d9ad77c54192bba883e0aa918c8e4ba245b265
dakofds <156973927+dakofds@users.noreply.github.com>
2025-08-14 19:28:01 -0300

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,26 @@
+require 'httparty'
+
+API_KEY = '2a353cfa63bb0e6da0be4292'
+
+def converter_moeda(de, para, valor)
+  url = "https://v6.exchangerate-api.com/v6/#{API_KEY}/pair/#{de}/#{para}/#{valor}"
+  resposta = HTTParty.get(url)
+
+  if resposta.code == 200 && resposta['result'] == 'success'
+    convertido = resposta['conversion_result']
+    taxa = resposta['conversion_rate']
+    puts "#{valor} #{de} = #{'%.2f' % convertido} #{para} (Taxa: #{'%.4f' % taxa})"
+  else
+    puts "Erro na conversão: #{resposta['error-type'] || 'Erro desconhecido'}"
+  end
+end
+
+puts "Converter de qual moeda?"
+de = gets.chomp
+puts "Para qual?"
+para = gets.chomp
+puts "Qual valor?"
+quantia = gets.chomp.to_i
+
+converter_moeda(de, para, quantia)
+
